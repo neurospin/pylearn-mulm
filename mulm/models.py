71034955581ee8f8391cd83383d2cbcec09bd7d2
jinpeng <mr.li.jinpeng@gmail.com>
2013-09-11 17:45:06 +0200

    ENH: mulm regression

--- mulm/models.py
+++ mulm/models.py
@@ -141,6 +141,7 @@ class MUOLS:
             p_vals = stats.f.sf(f_stats, df_c1, df_res)
             return f_stats, p_vals
 
+
 class MURidgeLM:
     """Mass-univariate linear modeling based on Ridge regression.
     Fit independant Ridge models for each columns of Y."""
@@ -154,6 +155,184 @@ class MURidgeLM:
     def predict(self, X):
         X = safe_asarray(X)
         return np.dot(X, self.coef_)
+
+
+from epac.workflow.splitters import BaseNodeSplitter
+from epac.workflow.splitters import Slicer
+from epac.workflow.splitters import VirtualList
+from epac.workflow.factory import NodeFactory
+from epac.configuration import conf
+
+
+class MULMColSlicer(Slicer):
+    """Row-wise reslicing of the downstream blocs.
+
+    Parameters
+    ----------
+    name: string
+
+    apply_on: string or list of strings (or None)
+        The name(s) of the downstream blocs to be rescliced. If
+        None, all downstream blocs are rescliced.
+    """
+
+    def __init__(self, signature_name, nb, apply_on):
+        super(self.__class__, self).__init__(signature_name, nb)
+        self.slices = None
+        self.n = 0  # the dimension of that array in ds should respect
+        if not apply_on:  # None is an acceptable value here
+            self.apply_on = apply_on
+        elif isinstance(apply_on, list):
+            self.apply_on = apply_on
+        elif isinstance(apply_on, str):
+            self.apply_on = [apply_on]
+        else:
+            raise ValueError("apply_on must be a string or a "\
+                "list of strings or None")
+
+    def set_sclices(self, slices):
+        # convert as a list if required
+        if isinstance(slices, dict):
+            self.slices =\
+                {k: slices[k].tolist() if isinstance(slices[k], np.ndarray)
+                else slices[k] for k in slices}
+            self.n = np.sum([len(v) for v in self.slices.values()])
+        else:
+            self.slices = \
+                slices.tolist() if isinstance(slices, np.ndarray) else slices
+            self.n = len(self.slices)
+
+    def transform(self, **Xy):
+        if not self.slices:
+            raise ValueError("Slicing hasn't been initialized. "
+            "Slicers constructors such as CV or Perm should be called "
+            "with a sample. Ex.: CV(..., y=y), Perm(..., y=y)")
+        data_keys = self.apply_on if self.apply_on else Xy.keys()
+        for data_key in data_keys:  # slice input data
+            dat = Xy.pop(data_key)
+            if len(dat.shape) == 2:
+                Xy[data_key] = dat[:, self.slices[data_key]]
+            else:
+                Xy[data_key] = dat[self.slices[data_key]]
+        return Xy
+
+
+class MULM(BaseNodeSplitter):
+    """Cross-validation parallelization.
+
+    Parameters
+    ----------
+    node: Node | Estimator
+        Estimator: should implement fit/predict/score function
+        Node: Pipe | Par*
+
+    n_folds: int
+        Number of folds. (Default 5)
+
+    cv_type: string
+        Values: "stratified", "random", "loo". Default "stratified".
+
+    random_state : int or RandomState
+        Pseudo-random number generator state used for random sampling.
+
+    reducer: Reducer
+        A Reducer should implement the reduce(node, key2, val) method.
+        Default ClassificationReport() with default arguments.
+    
+    Example
+    -------
+    >>> from sklearn import datasets
+    >>> from sklearn.svm import SVC
+    >>> from epac import Methods
+    >>> from epac.workflow.splitters import CVBestSearchRefitParallel
+
+    """
+
+    def __init__(self, node, x_group_indices, y_group_indices, **kwargs):
+        super(self.__class__, self).__init__()
+
+        self.x_group_indices = x_group_indices
+        self.y_group_indices = y_group_indices
+        self.slicer = MULMColSlicer(
+            signature_name=self.__class__.__name__,\
+            nb=0,\
+            apply_on=None)
+
+        self.x_uni_group_indices = set(x_group_indices)
+        self.y_uni_group_indices = set(y_group_indices)
+        size = len(self.x_uni_group_indices) * len(self.y_uni_group_indices)
+        self.children = VirtualList(size=size, parent=self)
+        self.slicer.parent = self
+
+        subtree = NodeFactory.build(node)
+        # subtree = node if isinstance(node, BaseNode) else LeafEstimator(node)
+        self.slicer.add_child(subtree)
+
+    def move_to_child(self, nb):
+        self.slicer.set_nb(nb)
+        cpt = 0
+        cp_x_uni_group_indice = None
+        cp_y_uni_group_indice = None
+        for x_uni_group_indice in self.x_uni_group_indices:
+            if (not cp_x_uni_group_indice == None)\
+                or (not cp_y_uni_group_indice == None):
+                break
+            for y_uni_group_indice in self.y_uni_group_indices:
+                if cpt == nb:
+                    cp_x_uni_group_indice = x_uni_group_indice
+                    cp_y_uni_group_indice = y_uni_group_indice
+                    break
+                cpt += 1
+        if (not cp_x_uni_group_indice == None)\
+                or (not cp_y_uni_group_indice == None):
+            x_indices = np.nonzero(np.asarray(self.x_group_indices) == \
+                            np.asarray(cp_x_uni_group_indice))
+            x_indices = x_indices[0]
+            y_indices = np.nonzero(np.asarray(self.y_group_indices) == \
+                            np.asarray(cp_y_uni_group_indice))
+            y_indices = y_indices[0]
+            self.slicer.set_sclices({"X": x_indices,
+                                     "Y": y_indices})
+        return self.slicer
+
+    def transform(self, **Xy):
+        self._sclices = None
+        return Xy
+
+    def get_parameters(self):
+        return dict(n_folds=self.n_folds)
+
+
+if __name__ == "__main__":
+    import numpy as np
+    import random
+    from mulm.models import MULM
+    from sklearn import datasets
+    from sklearn.svm import SVC
+    from epac import Methods
+    from epac.workflow.splitters import CVBestSearchRefitParallel
+
+    n_samples = 10
+    n_xfeatures = 20
+    n_yfeatures = 15
+    x_n_groups = 3
+    y_n_groups = 2
+
+    X = np.random.randn(n_samples, n_xfeatures)
+    Y = np.random.randn(n_samples, n_yfeatures)
+    x_group_indices = np.array([random.randint(0, x_n_groups)\
+        for i in xrange(n_xfeatures)])
+    y_group_indices = np.array([random.randint(0, y_n_groups)\
+        for i in xrange(n_yfeatures)])
+
+    mulm = MULM(MUOLS(), x_group_indices, y_group_indices)
+    ret_XY = mulm.run(X=X, Y=Y)
+    print "==================ret_XY======================"
+    print ret_XY
+    for leaf in mulm.walk_leaves():
+        print "===============leaf.load_results()================="
+        print "key =", leaf.get_key()
+        print leaf.load_results()
 
 #class OLSRegression:
 #    def __init__(self):
