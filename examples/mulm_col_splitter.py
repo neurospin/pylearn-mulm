faa7ee6c689da6d1fa1823cfeab81367464faebd
jinpeng <mr.li.jinpeng@gmail.com>
2013-09-18 14:47:43 +0200

    DOC: add examples

---
+++ examples/mulm_col_splitter.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 14:06:51 2013
+
+@author: jinpeng.li@cea.fr
+"""
+
+
+import numpy as np
+import random
+from epac import LocalEngine, SomaWorkflowEngine
+from epac import CRSplitter
+from mulm import MUOLS, MUOLSStats
+
+if __name__ == "__main__":
+    n_samples = 10
+    n_xfeatures = 20
+    n_yfeatures = 15
+    x_n_groups = 3
+    y_n_groups = 2
+
+    X = np.random.randn(n_samples, n_xfeatures)
+    Y = np.random.randn(n_samples, n_yfeatures)
+    x_group_indices = np.array([random.randint(0, x_n_groups)\
+        for i in xrange(n_xfeatures)])
+
+    # 1) Prediction for each X block return a n_samples x n_yfeatures
+    mulm = CRSplitter(MUOLS(),
+                      {"X": x_group_indices},
+                      col_or_row=True)
+    mulm.run(X=X, Y=Y)
+    for leaf in mulm.walk_leaves():
+        print "===============leaf.load_results()================="
+        print "key =", leaf.get_key()
+        tab = leaf.load_results()
+        print tab["MUOLS"]['Y/pred']
+
+    # 2) Prediction for each X block return a n_samples x n_yfeatures
+    mulm_stats = CRSplitter(MUOLSStats(),
+                            {"X": x_group_indices},
+                            col_or_row=True)
+    # ========================================
+    # Signle process
+    # mulm_stats.run(X=X, Y=Y)
+    # ========================================
+    # To use local-multi porcessing
+    local_engine = LocalEngine(tree_root=mulm_stats,
+                               num_processes=2)
+    mulm_stats = local_engine.run(X=X, Y=Y)
+    # ========================================
+    # To use soma-workflow
+#    swf_engine = SomaWorkflowEngine(tree_root=mulm_stats,
+#                               num_processes=2)
+#    mulm_stats = swf_engine.run(X=X, Y=Y)
+    for leaf in mulm_stats.walk_leaves():
+        print "===============leaf.load_results()================="
+        print "key =", leaf.get_key()
+        tab = leaf.load_results()
+        print tab["MUOLSStats"]
---
+++ examples/permutations.py
@@ -0,0 +1,52 @@
+"""
+Permutation example using epac
+
+@author: jinpeng.li@cea.fr
+
+"""
+
+import numpy as np
+import random
+from epac import LocalEngine, SomaWorkflowEngine
+from epac import Perms
+from epac import CRSplitter
+from mulm import MUOLSYR2
+from mulm import PValR2Reducer
+
+
+if __name__ == "__main__":
+
+    n_samples = 10
+    n_xfeatures = 20
+    n_yfeatures = 15
+    x_n_groups = 3
+    y_n_groups = 2
+
+    X = np.random.randn(n_samples, n_xfeatures)
+    Y = np.random.randn(n_samples, n_yfeatures)
+    X_group_indices = np.array([random.randint(0, x_n_groups)\
+        for i in xrange(n_xfeatures)])
+
+    col_splitter = CRSplitter(MUOLSYR2(),
+                              {"X": X_group_indices},
+                              col_or_row=True)
+#    col_splitter.run(X=X, Y=Y)
+#    print col_splitter.reduce()
+    row_perms = Perms(col_splitter,
+                  n_perms=10,
+                  reducer=PValR2Reducer(),
+                  permute="X",
+                  col_or_row=False,
+                  need_group_key=False)
+    row_perms.run(X=X, Y=Y)
+    # ========================================
+    # To use local-multi porcessing
+#    local_engine = LocalEngine(tree_root=row_perms,
+#                               num_processes=2)
+#    row_perms = local_engine.run(X=X, Y=Y)
+    # ========================================
+    # To use soma-workflow
+#    swf_engine = SomaWorkflowEngine(tree_root=row_perms,
+#                               num_processes=2)
+#    row_perms = swf_engine.run(X=X, Y=Y)
+    print row_perms.reduce()
