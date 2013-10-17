87fb4fbc50b6c94de714cd2f809ba127dd624119
duchesnay <edouard.duchesnay@gmail.com>
2013-10-17 12:34:05 +0200

    REFAC: examples

--- examples/ols_permutations_with_epac.py
+++ examples/ols_permutations_with_epac.py
@@ -15,37 +15,28 @@ from mulm import PValR2Reducer
 
 
 if __name__ == "__main__":
-
+    # =============================================================================
+    # Generate data
     n_samples = 10
     n_xfeatures = 20
     n_yfeatures = 15
     x_n_groups = 3
     y_n_groups = 2
-
     X = np.random.randn(n_samples, n_xfeatures)
     Y = np.random.randn(n_samples, n_yfeatures)
     X_group_indices = np.array([random.randint(0, x_n_groups)\
         for i in xrange(n_xfeatures)])
 
+    # =============================================================================
+    # Build EPAC Workflow in two steps
     col_splitter = ColumnSplitter(MUOLSStatsPredictions(),
                               {"X": X_group_indices})
-    #    col_splitter.run(X=X, Y=Y)
-    #    print col_splitter.reduce()
     row_perms = Perms(col_splitter,
                   n_perms=10,
                   reducer=PValR2Reducer(),
                   permute="X",
                   col_or_row=False,
                   need_group_key=False)
-    row_perms.run(X=X, Y=Y)
-    # ========================================
-    # To use local-multi porcessing
-#    local_engine = LocalEngine(tree_root=row_perms,
-#                               num_processes=2)
-#    row_perms = local_engine.run(X=X, Y=Y)
-    # ========================================
-    # To use soma-workflow
-#    swf_engine = SomaWorkflowEngine(tree_root=row_perms,
-#                               num_processes=2)
-#    row_perms = swf_engine.run(X=X, Y=Y)
-    print row_perms.reduce()
+    row_perms.run(X=X, Y=Y)            # map
+    results = row_perms.reduce()       # reduce
+    print results
--- examples/ols_with_epac.py
+++ examples/ols_with_epac.py
@@ -13,55 +13,53 @@ from epac import ColumnSplitter
 from mulm import MUOLS, MUOLSStatsCoefficients, MUOLSStatsPredictions
 
 if __name__ == "__main__":
+    # =============================================================================
+    # Generate data
     n_samples = 10
     n_xfeatures = 20
     n_yfeatures = 15
     x_n_groups = 3
     y_n_groups = 2
-
     X = np.random.randn(n_samples, n_xfeatures)
     Y = np.random.randn(n_samples, n_yfeatures)
-    x_group_indices = [0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3,
-                       4, 4, 4, 4, 5, 5, 5]
-
-    blocks_size = [np.sum(lev == x_group_indices)
-                          for lev in set(x_group_indices)]
+    x_group_indices = [1, 1, 3, 2, 2, 2, 2]
+    x_group_indices = np.array([random.randint(0, x_n_groups)\
+        for i in xrange(n_xfeatures)])
+    blocks_size = [np.sum(lev == x_group_indices) for lev in set(x_group_indices)]
     print "Blocks size", blocks_size
 
+    # =============================================================================
     # 1) Prediction for each X block return a n_samples x n_yfeatures
-    mulm = ColumnSplitter(MUOLS(),
-                          {"X": x_group_indices})
-    mulm.run(X=X, Y=Y)
-    for leaf in mulm.walk_leaves():
-        print "===============leaf.load_results()================="
-        print "key =", leaf.get_key()
-        tab = leaf.load_results()
+    mulm = ColumnSplitter(MUOLS(), {"X": x_group_indices})  # EPAC workflow
+    mulm.run(X=X, Y=Y)                                      # map
+    res_tab = mulm.reduce()                                 # reduce
+    for res_tab_key in res_tab.keys():
+        print "===============key=%s=============" % res_tab_key
         print "predictions shape == (n_samples, n_yfeatures) ?:",\
-            tab["MUOLS"]['Y/pred'].shape == (n_samples, n_yfeatures)
+            res_tab[res_tab_key]['Y/pred'].shape == (n_samples, n_yfeatures)
 
+    # =============================================================================
     # 2) coeficient Statistics for each block (of size k) return
     # a block_size x n_yfeatures array of t-values and p-values
     mulm_stats_coefficients = ColumnSplitter(MUOLSStatsCoefficients(),
-                                {"X": x_group_indices})
-    mulm_stats_coefficients.run(X=X, Y=Y)
+                                {"X": x_group_indices})  # EPAC workflow
+    mulm_stats_coefficients.run(X=X, Y=Y)                # map
+    res_tab = mulm_stats_coefficients.reduce()           # reduce
     k = 0
-    for leaf in mulm_stats_coefficients.walk_leaves():
-        print "===============leaf.load_results()================="
-        print "key =", leaf.get_key()
-        tab = leaf.load_results()
+    for res_tab_key in res_tab.keys():
+        print "===============key=%s=============" % res_tab_key
         print "p-values shape: == (k, n_yfeatures) ?:",\
-            tab["MUOLSStatsCoefficients"]['pvals'].shape == (blocks_size[k],
-                                                             n_yfeatures)
+            res_tab[res_tab_key]['pvals'].shape == (blocks_size[k], n_yfeatures)
         k += 1
 
+    # =============================================================================
     # 3) Prediction statistics for each block (of size k) return
-    # a block_size x n_yfeatures array of r2
+    # a block_size x n_yfeatures array of t-values and p-values
     mulm_stats_predictions = ColumnSplitter(MUOLSStatsPredictions(),
-                                {"X": x_group_indices})
-    mulm_stats_predictions.run(X=X, Y=Y)
-    for leaf in mulm_stats_predictions.walk_leaves():
-        print "===============leaf.load_results()================="
-        print "key =", leaf.get_key()
-        tab = leaf.load_results()
+                                {"X": x_group_indices})  # EPAC workflow
+    mulm_stats_predictions.run(X=X, Y=Y)                 # map
+    res_tab = mulm_stats_predictions.reduce()            # reduce
+    for res_tab_key in res_tab.keys():
+        print "===============key=%s=============" % res_tab_key
         print "p-values shape: == n_yfeatures ?:",\
-            tab["MUOLSStatsPredictions"]['r2'].shape == (n_yfeatures, )
+            res_tab[res_tab_key]['r2'].shape == (n_yfeatures, )
