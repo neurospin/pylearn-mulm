f9dffec3ad5018740cfa54f2228c09a17cc8a420
Mathieu Dubois <mathieu.dubois@cea.fr>
2014-07-08 10:33:38 +0200

    BUG: removed old classes from __init__.py (was throwing an ImportError exception).

--- mulm/__init__.py
+++ mulm/__init__.py
@@ -8,11 +8,7 @@ Created on Fri Jun 14 19:38:46 2013
 from .models import MUPairwiseCorr
 from .models import MUOLS
 from .models import MUOLSStatsCoefficients
-from .models import MUOLSStatsPredictions
-from .reducers import PValR2Reducer
 
 __all__ = ['MUPairwiseCorr',
            'MUOLS',
-           'MUOLSStatsCoefficients',
-           'MUOLSStatsPredictions',
-           'PValR2Reducer']
+           'MUOLSStatsCoefficients']
